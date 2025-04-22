1e8b2ea0323bbd6640a3595f84117280c75df15c
FLCKE <louisfrancisco229@gmail.com>
2025-04-22 15:57:54 +0200

    resolve deployement problems

--- backend/src/routes/visioCallingRoutes.js
+++ backend/src/routes/visioCallingRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { verifyToken } = require("../middlewares/authMiddleware");
-const { createRoomController } = require("../controllers/VisioCallingController.js");
+const { createRoomController } = require("../controllers/visioCallingController.js");
 const router = express.Router();
 
 router.post("/create-room" , createRoomController);
