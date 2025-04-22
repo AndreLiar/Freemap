dc28c5ee77fdaf6a3d6f6aa33d2ea615b203f402
FLCKE <louisfrancisco229@gmail.com>
2025-04-22 15:48:08 +0200

    resolve deployement problems

--- backend/src/controllers/visioCallingController.js
+++ backend/src/controllers/visioCallingController.js
@@ -1,17 +1,17 @@
+//src/controllers/visioCallingController.js
+const { v4: uuidv4 } = require("uuid");
 
-    const { v4: uuidv4 } = require("uuid");
+// Route pour créer une salle Jitsi
+const createRoomController = async (req, res) => {
+    try {
+        console.log("Création de la salle");
+        const roomId = uuidv4(); // Générer un ID de salle unique
 
-    // Route pour créer une salle Jitsi
-    const createRoomController = async (req, res) => {
-        try {
-            console.log("Création de la salle");
-            const roomId = uuidv4(); // Générer un ID de salle unique
-            
-            res.status(200).json({ roomId });
-        } catch (error) {
-            res.status(500).json({ message: "Erreur lors de la création de la salle" });
-        }
-    };
+        res.status(200).json({ roomId });
+    } catch (error) {
+        res.status(500).json({ message: "Erreur lors de la création de la salle" });
+    }
+};
 
 
 // Exporter la fonction sous forme d'objet
